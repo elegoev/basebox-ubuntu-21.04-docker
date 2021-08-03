0a1cf5278d9d36e46a85a33d14bcab06170bee8e
Urs Voegele <unext@bluewin.ch>
2021-08-03 17:18:49 +0200

    new basebox version 4.0.1628003296

---
+++ installed-application.md
@@ -0,0 +1,3 @@
+# Installed application 
+***                     
+> docker 20.10.7
--- set-build-env.ps1
+++ set-build-env.ps1
@@ -1,4 +1,4 @@
 
-$env:SRC_IMAGE_NAME = 'elegoev/ubuntu-18.04-docker'
-$env:SRC_IMAGE_VERSION = '4.0.1614790270'
+$env:SRC_IMAGE_NAME = 'elegoev/ubuntu-21.04-docker'
+$env:SRC_IMAGE_VERSION = '4.0.1628003296'
 
